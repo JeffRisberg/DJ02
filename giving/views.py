5faa0b7ed6bf95db4a3b8e3ca6cb4dc5ad91222e
JeffRisberg <jrisberg@aol.com>
2017-09-12 09:15:27 -0700

    added data model, and charities list template

--- giving/views.py
+++ giving/views.py
@@ -1,5 +1,17 @@
 from django.http import HttpResponse
 
+from django.http.response import HttpResponse
+
+from django.template import Context, loader
+
+from .models import Charity
 
 def index(request):
     return HttpResponse("Hello, world. You're at the giving index.")
+
+def homepage(request):
+    charity_list = Charity.objects.all()
+    template = loader.get_template('giving/charity_list.html')
+    context = Context({'charity_list': charity_list})
+    output = template.render(context)
+    return HttpResponse(output)
