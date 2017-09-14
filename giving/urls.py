c62c724c39b522ed5ba0fe4e715b714bce4df15f
JeffRisberg <jrisberg@aol.com>
2017-09-14 15:30:35 -0700

    added DonorList

--- giving/urls.py
+++ giving/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^charityList$', views.charity_list_view, name='charity_list_view'),
+    url(r'^donorList$', views.donor_list_view, name='donor_list_view'),
     url(r'^$', views.index, name='index'),
 ]
--- giving/views.py
+++ giving/views.py
@@ -2,14 +2,23 @@ from django.http.response import HttpResponse
 
 from django.template import Context, loader
 
-from .models import Charity
+from .models import Charity, Donor
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the giving index.")
+    return HttpResponse("Hello, Welcome to the Giving Application")
+
 
 def charity_list_view(request):
     charity_list = Charity.objects.all()
     template = loader.get_template('giving/charity_list.html')
     context = Context({'charity_list': charity_list})
+    output = template.render(context)
+    return HttpResponse(output)
+
+
+def donor_list_view(request):
+    donor_list = Donor.objects.all()
+    template = loader.get_template('giving/donor_list.html')
+    context = Context({'donor_list': donor_list})
     output = template.render(context)
     return HttpResponse(output)
